a274d195505db327b85ca66eae851a018aa40727
Mirko-A <adzicmirko97@gmail.com>
2025-12-29 16:43:09 +0100

    systemc hello world

---
+++ systemc-hello/hello.cpp
@@ -0,0 +1,16 @@
+#include <systemc>
+#include <iostream>
+
+SC_MODULE (hello)
+{
+    SC_CTOR (hello)
+    {
+        std::cout << "Hello\n";
+    }
+};
+
+int sc_main(int argc, char∗ argv[])
+{
+    hello h("hello");
+    return (0);
+}
